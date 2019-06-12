48b6d5a680a3b8948ef113701f1a5afc461c5fa9
xkong <kong.xu@husky.meu.edu>
2019-06-12 17:29:03 -0400

    finished widget serivice in server side

--- src/main/java/com/example/wbdvsu19xuankongmyappassignment5/controller/WidgetController.java
+++ src/main/java/com/example/wbdvsu19xuankongmyappassignment5/controller/WidgetController.java
@@ -1,6 +1,7 @@
 package com.example.wbdvsu19xuankongmyappassignment5.controller;
 
 import com.example.wbdvsu19xuankongmyappassignment5.models.Widget;
+import com.example.wbdvsu19xuankongmyappassignment5.services.WidgetService;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,66 +21,44 @@ import java.util.List;
 
 @RestController
 public class WidgetController {
-  private static List<Widget> widgets = new ArrayList<>();
 
-  static {
-    widgets.add(new Widget(123, Widget.Type.HEADING, "Widget 1", "1", "heading demo text", "", true));
-    widgets.add(new Widget(234, Widget.Type.PARAGRAPH, "Widget 2", "2", "para demo text", "", true));
-    widgets.add(new Widget(345, Widget.Type.IMAGE, "Widget 3", "2", "img demo text", "https://northeastern.blackboard.com/branding/_1_1/NortheasternUniversityBlackboardBannerGray.png", true));
-    widgets.add(new Widget(567, Widget.Type.LINK, "Widget 4", "2", "link demo text", "https://northeastern.blackboard.com/", true));
-    widgets.add(new Widget(789, Widget.Type.LIST, "Widget 5", "2", "Hello World", "", false));
+  WidgetService widgetService;
+
+  WidgetController() {
+    widgetService = new WidgetService();
   }
 
-
   @PostMapping("/api/widgets")
-  public Widget createWidget(
+  public List<Widget> createWidget(
           @RequestBody Widget widget) {
-    widgets.add(widget);
-    return widget;
+    widgetService.createWidget(widget);
+    return widgetService.findAllWigets();
   }
 
 
   @GetMapping("/api/widgets")
   public List<Widget> findAllWigets(Widget widget) {
-    return widgets;
+
+    return widgetService.findAllWigets();
   }
 
 
   @GetMapping("/api/widgets/{widgetId}")
   public Widget findWidgetById(@PathVariable("widgetId") Integer id) {
-    for (Widget widget : widgets) {
-      if (widget.getId().equals(id)) {
-        return widget;
-      }
-    }
-    return null;
+
+    return widgetService.findWidgetById(id);
   }
 
   @PutMapping("/api/widgets/{widgetId}")
-  public Widget updateWidget(
+  publ  ic Widget updateWidget(
           @PathVariable("widgetId") Integer wid,
           @RequestBody Widget newWidget) {
-
-    for (Widget widget : widgets) {
-      if (widget.getId().equals(wid)) {
-        widget.setWidgetType(newWidget.getWidgetType());
-        widget.setName(newWidget.getName());
-        widget.setSize(newWidget.getSize());
-        widget.setText(newWidget.getText());
-        widget.setSrc(newWidget.getSrc());
-        widget.setOrder(newWidget.getOrder());
-        return newWidget;
-      }
-    }
-    return null;
+    return widgetService.updateWidget(wid, newWidget);
   }
 
   @DeleteMapping("/api/widgets/{widgetId}")
   public void deleteWidget(@PathVariable("widgetId") Integer wid) {
-    for (int i = 0; i < widgets.size(); i++) {
-      if (widgets.get(i).getId().equals(wid)){
-        widgets.remove(i);
-      }
-    }
+
+    widgetService.deleteWidget(wid);
   }
 }
--- src/main/java/com/example/wbdvsu19xuankongmyappassignment5/services/WidgetService.java
+++ src/main/java/com/example/wbdvsu19xuankongmyappassignment5/services/WidgetService.java
@@ -1,8 +1,68 @@
 package com.example.wbdvsu19xuankongmyappassignment5.services;
 
+import com.example.wbdvsu19xuankongmyappassignment5.models.Widget;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Xuan Kong
  * @Date 2019-06-11.
  */
 public class WidgetService {
+
+  public static List<Widget> widgets = new ArrayList<>();
+
+
+  static {
+    widgets.add(new Widget(123, Widget.Type.HEADING, "Widget 1", "1", "heading demo text", "", true));
+    widgets.add(new Widget(234, Widget.Type.PARAGRAPH, "Widget 2", "2", "para demo text", "", true));
+    widgets.add(new Widget(345, Widget.Type.IMAGE, "Widget 3", "2", "img demo text", "https://northeastern.blackboard.com/branding/_1_1/NortheasternUniversityBlackboardBannerGray.png", true));
+    widgets.add(new Widget(567, Widget.Type.LINK, "Widget 4", "2", "link demo text", "https://northeastern.blackboard.com/", true));
+    widgets.add(new Widget(789, Widget.Type.LIST, "Widget 5", "2", "Hello World", "", false));
+  }
+
+  public List<Widget> createWidget(Widget widget) {
+    widgets.add(widget);
+    return widgets;
+  }
+
+
+  public List<Widget> findAllWigets() {
+    return widgets;
+  }
+
+
+  public Widget findWidgetById(Integer id) {
+    for (Widget widget : widgets) {
+      if (widget.getId().equals(id)) {
+        return widget;
+      }
+    }
+    return null;
+  }
+
+  public Widget updateWidget(Integer wid, Widget newWidget) {
+
+    for (Widget widget : widgets) {
+      if (widget.getId().equals(wid)) {
+        widget.setWidgetType(newWidget.getWidgetType());
+        widget.setName(newWidget.getName());
+        widget.setSize(newWidget.getSize());
+        widget.setText(newWidget.getText());
+        widget.setSrc(newWidget.getSrc());
+        widget.setOrder(newWidget.getOrder());
+        return newWidget;
+      }
+    }
+    return null;
+  }
+
+  public void deleteWidget(Integer wid) {
+    for (int i = 0; i < widgets.size(); i++) {
+      if (widgets.get(i).getId().equals(wid)) {
+        widgets.remove(i);
+      }
+    }
+  }
 }
